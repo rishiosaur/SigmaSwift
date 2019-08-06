741b37948f607b4f78f6a00bd4c1d565cc2880a5
Rishi Kothari <itsrishikothari@gmail.com>
2019-08-06 15:27:09 -0400

    Fixed Cocoa Abstraction problem

--- Operations/Constant.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Constant.swift
-//  Nimble
-//
-//  Created by Rishi Kothari on 2019-08-06.
-//
-
--- Variable.swift
+++ Variable.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+let a = Variable(coefficient: <#T##Int#>, operation: <#T##Operation#>, value: <#T##String#>)
 /**
  
  */
