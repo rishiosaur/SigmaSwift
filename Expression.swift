7b3fbecd4f0ed021006c5ef75be21558277cb28f
Rishi Kothari <itsrishikothari@gmail.com>
2019-08-06 08:51:09 -0400

    Protocols and basic structs with docs

---
+++ Expression.swift
@@ -0,0 +1,8 @@
+//
+//  Expression.swift
+//  Nimble
+//
+//  Created by Rishi Kothari on 2019-08-06.
+//
+
+import Foundation
--- SigmaSwift/Classes/Basics.swift
+++ SigmaSwift/Classes/Basics.swift
@@ -1,3 +1,8 @@
+/**
+An `enum` to describe possible operations on a `Variable` or `Constant`.
+ 
+The `rawValue` of each `case` will be a `String` corresponding to that `Operation`.
+*/
 enum Operation : String {
   case add = "+"
   case subtract = "-"
@@ -6,12 +11,35 @@ enum Operation : String {
   case divide = "/"
 }
 
-struct Variable {
+///**
+// The `Coefficient` is a stripped-away `Variable` or `Constant`, with a single value, although this is prone to changes.
+//
+// For now, the `Coefficient` will only be able to hold a string value, as part of a variable. If a more complex term is required, like 2xy, then one could just multiply another variable `y` by `2x` to acheive the same result.
+// */
+//struct Coefficient {
+//  var value: Int
+//}
+
+/**
+ # Variables and Constants
+ These are the underpinnings of math, but in `Sigma`, they are rather simple (as of 5/8/19).
+ For now, they consist of an operation, and a value.
+ In a Variable
+ */
+
+protocol Term {
+  
+}
+
+/// A `Variable` is part of a Term, and consists of an `operation`; and a `value`, which may change at any time.
+struct Variable : Term {
+  
   var operation : Operation?
   var value : String
 }
 
-struct Constant {
+/// A `Constant` is part of a Term, and consists of an `operation`; and a `value`, which will stay constant at all times.
+struct Constant : Term {
   var operation : Operation?
   var value: Int
 }
